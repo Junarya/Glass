0d120fd8f9aea9cb0c326c5d2805837f46118b43
Junarya <115864499+Junarya@users.noreply.github.com>
2023-02-18 19:18:33 +0300

    Simple

---
+++ js/script.js
@@ -0,0 +1,19 @@
+ 
+
+  window.onscroll = function() {scrollFunction()};
+        
+  function scrollFunction() {
+      if (document.body.scrollTop > 200 || 
+        document.documentElement.scrollTop > 200) {
+          document.getElementById("topBtn").style.display = "block";
+      } else {
+          document.getElementById("topBtn").style.display = "none";
+      }
+  }
+  
+  function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  }  
+
+  
